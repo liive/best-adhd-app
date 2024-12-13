67fa5e4d38e0ca7b7af1b21fd52ed79987ea18e1
liive <livetab@gmail.com>
2024-12-13 20:24:02 +0200

    updated email section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import { AppList } from './components/AppList/AppList';
+import { Newsletter } from './components/Newsletter/Newsletter';
 import { apps } from './data/apps';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
-      <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12">
+        <Newsletter />
         <AppList apps={apps} />
       </main>
       <Footer />
